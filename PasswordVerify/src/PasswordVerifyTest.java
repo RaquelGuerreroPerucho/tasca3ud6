118c6c471d31127487dc1d3a5f036753cf708a98
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-15 17:11:10 +0200

    Commit 4. Test 4,5,6 valido. Junto porque se me ha olvidado.

--- PasswordVerify/src/PasswordVerifyTest.java
+++ PasswordVerify/src/PasswordVerifyTest.java
@@ -44,4 +44,42 @@ class PasswordVerifyTest {
 
         assertEquals(resultadoEsperado, resultadoReal);
     }
+
+    @Test
+    public void test4() {
+        String resultadoEsperado = "false" +
+                "\n" + "La contrasenya ha de tenir almenys 8 caràcters" +
+                "\n" + "La contrasenya ha de contenir almenys un caràcter especial" +
+                "\n" + "La contrasenya ha de contenir almenys una lletra majúscula";
+
+        String resultadoReal = PasswordVerify.passwordVerificator("hola12");
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test5() {
+        String resultadoEsperado = "false" +
+                "\n" + "La contrasenya ha de tenir almenys 8 caràcters" +
+                "\n" + "La contrasenya ha de contenir almenys 2 números"+
+                "\n" + "La contrasenya ha de contenir almenys un caràcter especial";
+
+
+        String resultadoReal = PasswordVerify.passwordVerificator("holA");
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void test6() {
+        String resultadoEsperado = "false" +
+                "\n" + "La contrasenya ha de tenir almenys 8 caràcters" +
+                "\n" + "La contrasenya ha de contenir almenys 2 números"+
+                "\n" + "La contrasenya ha de contenir almenys una lletra majúscula";
+
+
+        String resultadoReal = PasswordVerify.passwordVerificator("hola!");
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
 }
